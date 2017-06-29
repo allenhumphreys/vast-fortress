b7ee28a293a24c94bced499b4739407d3227e6b0
Allen Humphreys <allen.humphreys@icloud.com>
2017-06-29 12:54:42 -0500

    Fix missed files

---
+++ api/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import include, url
+
+#Rest Framework
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format)
+    })
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    url(r'^api/?$', api_root),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
