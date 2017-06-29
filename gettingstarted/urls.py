b75b21e3a3ed08cb21d2a25bddc4594a81fa3162
Allen Humphreys <allen.humphreys@icloud.com>
2017-06-29 13:43:30 -0500

    Fixes URLs

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,6 +12,6 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db/?', hello.views.db, name='db'),
-    url(r'^admin/?', include(admin.site.urls)),
-    url(r'^api/?', include('api.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('api.urls')),
 ]
